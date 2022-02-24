31c852e87c02723473bca9ab436431277e318ed6
danial921 <danialfarrosmaulana921@gmail.com>
2022-02-24 22:14:06 +0700

    add route

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,16 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         return auth()->user()->name;
     });
 
-    Route::resource('petrosmartform', App\Http\Controllers\API\PetrosmartformsController::class);    
+    Route::post('/oilrigdesignregistration', [App\Http\Controllers\API\OilrigformsController::class,'store']);
+    Route::post('/fracturingfluiddesignregistration', [App\Http\Controllers\API\FracturingfluidformsController::class,'store']);
+    Route::post('/stocktradingregistration', [App\Http\Controllers\API\StocktradingformsController::class,'store']);
+    Route::post('/petrosmartregistration', [App\Http\Controllers\API\PetrosmartformsController::class,'store']);
+    Route::post('/paperregistration', [App\Http\Controllers\API\PaperformsController::class,'store']);
+    Route::post('/businesscaseregistration', [App\Http\Controllers\API\BusinesscaseformsController::class,'store']);
+    Route::post('/casestudyregistration', [App\Http\Controllers\API\CasestudyformsController::class,'store']);
+    Route::post('/careertalksregistration', [App\Http\Controllers\API\PetrolidacareertalksController::class,'store']);
+
+     
     
     // API route for logout user
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
