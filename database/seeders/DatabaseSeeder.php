46f6c0a287a42497ca14afd7e85578c7bac3c1e1
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-02-28 17:10:19 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = new User;
+
+        $user->name     = 'Petrolida Admin';
+        $user->email    = 'admin@petrolida.com';
+        $user->password = Hash::make('G4tauP4ssnya');
+
+        $user->save();
     }
 }
